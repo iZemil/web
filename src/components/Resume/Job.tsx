b89092da5b91aa0dd2e76a557043c8f46ca5b290
zemil <3zemil@gmail.com>
2025-10-25 10:41:26 +0300

    fix: add years to job preview

--- src/components/Resume/Job.tsx
+++ src/components/Resume/Job.tsx
@@ -1,8 +1,28 @@
-import { differenceInMonths, format } from 'date-fns';
+import { format, intervalToDuration } from 'date-fns';
 import * as React from 'react';
 
 import { IJob } from './consts';
 import styles from './styles.module.css';
+
+function formatDateDifference(startDate: Date, endDate: Date = new Date()) {
+	const duration = intervalToDuration({ start: startDate, end: endDate });
+
+	const { years = 0, months = 0 } = duration;
+
+	if (years === 0 && months === 0) {
+		return '0 months';
+	}
+
+	if (years === 0) {
+		return `${months} month${months !== 1 ? 's' : ''}`;
+	}
+
+	if (months === 0) {
+		return `${years} year${years !== 1 ? 's' : ''}`;
+	}
+
+	return `${years} year${years !== 1 ? 's' : ''} and ${months} month${months !== 1 ? 's' : ''}`;
+}
 
 export const Job = (props: IJob) => {
 	const date = (d: Date) => format(d, 'MMM yyyy');
@@ -24,7 +44,7 @@ export const Job = (props: IJob) => {
 				{companyName}{' '}
 				<div className={styles.jobDates}>
 					{date(from)} – {date(to)}{' '}
-					<span className={styles.jobDatesDistance}>({differenceInMonths(to, from)} months)</span>
+					<span className={styles.jobDatesDistance}>({formatDateDifference(to, from)})</span>
 				</div>
 			</h4>
 			<div className={styles.jobDesc}>{projectDesc}</div>
